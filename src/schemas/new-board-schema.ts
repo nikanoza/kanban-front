f3ddaff41d451d9b92535c7d7224b467f1fba13f
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-17 08:53:58 +0400

    refactor: change schemas

--- src/schemas/new-board-schema.ts
+++ src/schemas/new-board-schema.ts
@@ -6,20 +6,12 @@ const newBoardSchema = z.object({
     .min(3)
     .refine((value) => value.trim().length > 0, {
       message: "Title can't be empty",
-    })
-    .refine((value) => /^[a-zA-Z\s]+$/.test(value), {
-      message: "Title must contain only English letters",
     }),
   columns: z
     .array(
-      z
-        .string()
-        .refine((value) => value.trim().length > 0, {
-          message: "Column can't be empty",
-        })
-        .refine((value) => /^[a-zA-Z\s]+$/.test(value), {
-          message: "Column must contain only English letters",
-        })
+      z.string().refine((value) => value.trim().length > 0, {
+        message: "Column can't be empty",
+      })
     )
     .default([]),
 });
